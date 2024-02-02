3f8706c605a283fdfedb87bdbe3b751b00bc0820
lmw7060 <dlaalsdn7@naver.com>
2024-02-02 11:38:08 +0900

    2nd

--- movie_recomandation.py
+++ movie_recomandation.py
@@ -18,10 +18,28 @@ Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
-ref_idx = 4
-print(df_reviews.iloc[ref_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
-print(cosine_sim[0])
-print(len(cosine_sim))
+# ref_idx = 4
+# print(df_reviews.iloc[ref_idx, 0])
+# cosine_sim = linear_kernel(Tfidf_matrix[ref_idx], Tfidf_matrix)
+# print(cosine_sim[0])
+# print(len(cosine_sim))
+# recommendation = getRecommendation(cosine_sim)
+# print(recommendation)
+
+#keyword이용
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+keyword = '마블'
+sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+words = [keyword]
+for word, _ in sim_word:
+    words.append(word)
+sentence = []
+count = 10
+for word in words:
+    sentence = sentence + [word] * count
+    count -= 1
+sentence = ' '.join(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
